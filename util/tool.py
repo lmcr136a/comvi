aff6a2872e4876285b32eb5d371288d7268c7fd5
wslee <ws.lee@saigeresearch.ai>
2021-11-02 19:40:30 +0900

    description added

--- util/tool.py
+++ util/tool.py
@@ -4,6 +4,11 @@ import shutil
 import yaml
 
 def configuration(config):
+    """
+    input: path of .yml file
+
+    output: dictionary of configurations
+    """
     with open(config, encoding="utf-8") as fp:
         cfg = yaml.load(fp, Loader=yaml.FullLoader)
 
