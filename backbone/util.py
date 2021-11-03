3c897824484a307f9f988bf6e5506ae0ead51288
wslee <ws.lee@saigeresearch.ai>
2021-11-03 15:07:03 +0900

    resnet을 기본으로 n_class랑 n_cv를 통해 네트워크 customize 가능

--- backbone/util.py
+++ backbone/util.py
@@ -1,5 +1,16 @@
-from .resnet import Resnet
+from .resnet import resnet18, resnet34, resnet50, resnet101, resnet152
 
-def getNetwork():
-    raise NotImplementedError
-    return None
+def getNetwork(cfg_network):
+    name = cfg_network["backbone"]
+    n_class = cfg_network["n_class"]
+    n_cv = cfg_network["n_cv"]
+    try:
+        return {
+            "resnet18": resnet18(n_class, n_cv),
+            "resnet34": resnet34(n_class, n_cv),
+            "resnet50": resnet50(n_class, n_cv),
+            "resnet101": resnet101(n_class, n_cv),
+            "resnet152": resnet152(n_class, n_cv),
+        }[name]
+    except:
+        raise (f"Model {name} not available")
